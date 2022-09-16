0e2e3156cf8d21e393816ca0406fdb2d3a6a4283
Vol.B <43251877+Volodymyr89@users.noreply.github.com>
2022-09-16 18:44:43 +0300

    added some functions

--- Track_Utilities.c
+++ Track_Utilities.c
@@ -1,7 +1,24 @@
+#include <stdint.h>
+#include <stdio.h>
 
+typedef struct Counters{
+    float counterwater;
+    float counterGas;
+    float counterElectrisity;
+}Counters_t;
 
 int main (){
 
+    void set_utility_values(void);
+
+    set_utility_values();
 
     return 0;
+}
+
+void set_utility_values(void){
+    printf("////////////////////////////////////////////////////////////////////////\n");
+    printf("////////////////////////////////////////////////////////////////////////\n\n");
+    printf("Показник Вода:\n");
+    scanf("%f", &counterwater)
 }
