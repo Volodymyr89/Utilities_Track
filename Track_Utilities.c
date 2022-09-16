d50766f27aae656f82169da2ddb17688afa4562b
Vol.B <43251877+Volodymyr89@users.noreply.github.com>
2022-09-16 19:08:37 +0300

    added new functions

--- Track_Utilities.c
+++ Track_Utilities.c
@@ -1,18 +1,43 @@
 #include <stdint.h>
 #include <stdio.h>
 
+uint8_t choise = 0;
 typedef struct Counters{
-    float counterwater;
+    float counterWater;
+    float counterWaterDistibution;
     float counterGas;
+    float counterGasDistibution;
     float counterElectrisity;
 }Counters_t;
 
+void showOptions(void);
+void set_utility_values(void);
+
 int main (){
 
-    void set_utility_values(void);
+    showOptions();
 
-    set_utility_values();
+   
+    while(1){
 
+        scanf("%d", &choise);
+        switch (choise)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;    
+        default:
+            break;
+        }
+
+
+    }
     return 0;
 }
 
@@ -20,5 +45,14 @@ void set_utility_values(void){
     printf("////////////////////////////////////////////////////////////////////////\n");
     printf("////////////////////////////////////////////////////////////////////////\n\n");
     printf("Показник Вода:\n");
-    scanf("%f", &counterwater)
+    //scanf("%f", &counterwater)
+}
+
+void showOptions(void){
+    printf("**************************************************************************************\n");
+    printf("**************************************************************************************\n\n");
+    printf("                           MENU:\n\n");
+    printf("Подивитись Показники Попередній Місяць натиснути - 1\n");
+    printf("Записати Показники натиснути - 2\n");
+    printf("Записати Оплату Показників натиснути - 3\n");
 }
