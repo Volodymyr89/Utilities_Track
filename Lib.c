4a43f176f4005d6a36c3b3577d5ca1df117e4fc5
Vol.B <43251877+Volodymyr89@users.noreply.github.com>
2022-09-19 12:56:44 +0300

    float number  corection

--- Lib.c
+++ Lib.c
@@ -55,8 +55,8 @@ void set_counters_all(void){
 
     fileptr = fopen( "C:/Learn_C/Utilities/Utilities_Track/Utilities_Data/File.txt", "r+");
     if (fileptr != NULL){
-      fprintf(fileptr, "%f\n", Counters.month_year);
-      fprintf(fileptr, "%.2f%.2f%.2f%.2f%.2f%.2f", Counters.counterWater, Counters.counterWaterDistibution, Counters.counterGas, Counters.counterGasDistibution, Counters.counterElectrisity, Counters.counterSofiivka);
+      fprintf(fileptr, "%.4f\n", Counters.month_year);
+      fprintf(fileptr, "%.1f%.1f%.1f%.1f%.1f%.1f", Counters.counterWater, Counters.counterWaterDistibution, Counters.counterGas, Counters.counterGasDistibution, Counters.counterElectrisity, Counters.counterSofiivka);
       
       fclose(fileptr);
       SetColorYellow();
